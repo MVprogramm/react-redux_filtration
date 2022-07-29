9b273190533547ae3034945a4085ae58289a3658
Michael Valiahmetov <michael.valiahmetov@gmail.com>
2022-07-29 19:07:18 +0300

    change input at Filter

--- src/Filter.jsx
+++ src/Filter.jsx
@@ -3,13 +3,17 @@ import React from 'react';
 import './filter.scss';
 
 const Filter =({ onChange, filterText, count }) => {
+  const handleInput = event => {
+    onChange(event.target.value);
+  };
+  
   return (
     <div className="filter">
       <span className="filter__count">{count}</span>
       <input 
         type="text" 
         className="filter__input"
-        onChange={() => onChange(event.target.value)} 
+        onChange={handleInput} 
         value={filterText} 
       />
     </div>
